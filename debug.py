a7869d4b9931269ec8c05c50b06d4b5a5989e748
Antero Escuder <antero.escuder.omenat.al@iespoligonosur.org>
2024-09-24 11:57:27 +0200

    ultimo commit

--- debug.py
+++ debug.py
@@ -1,17 +1,26 @@
+def sumar(a, b, iterations):
+    results = []
+    for i in range(iterations):
+        results.append(a + b)
+    return results
 
-def sumar(a, b):
-    return a + b
+def restar(a, b, iterations):
+    results = []
+    for i in range(iterations):
+        results.append(a - b)
+    return results
 
-def restar(a, b):
-    return a - b
+def multiplicar(a, b, iterations):
+    results = []
+    for i in range(iterations):
+        results.append(a * b)
+    return results
 
-def multiplicar(a, b):
-    return a * b
-
-def dividir(a, b):
-    if b == 0:
-        return "Error: División por cero"
-    return a / b
-
-
-       
+def dividir(a, b, iterations):
+    results = []
+    for i in range(iterations):
+        if b == 0:
+            results.append("Error: División por cero")
+        else:
+            results.append(a / b)
+    return results
